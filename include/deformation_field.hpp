0c8f6c0d48d8cff89223e3dc71d82d8e8a393f6d
Aditya-tumcse <srinivas.adi95@gmail.com>
2024-04-14 00:54:30 +0200

    feat : computing scalar basis functions for the scalar potential field

--- include/deformation_field.hpp
+++ include/deformation_field.hpp
@@ -8,11 +8,19 @@
 
 namespace adi{
     namespace deformation_field{
+
+        const std::vector<std::tuple<unsigned int>> getBasisFunctions(const int nunber_of_basis_functions);
         class deformationField{
             public:
                 deformationField(const std::vector<adi::Point> &source_point_cloud, const std::vector<adi::Point> &target_point_cloud, const std::vector<std::pair<adi::Point, adi::Point>> &correspondences);
+                
+                void initDeformationField(const std::vector<std::tuple<unsigned int, unsigned int, unsigned int>> &indices_for_basis_function_computation, const std::vector<adi::Point> &point_cloud);
 
                 Eigen::MatrixXd computeSoftCorrespondences();
+
+                const std::vector<adi::Point> getSourcePointCloud() const { return m_source_point_cloud;}
+
+                const std::vector<adi::Point> getTargetPointCloud() const {return m_target_point_cloud;}
 
             private:
                 const std::vector<adi::Point> &m_source_point_cloud;
@@ -22,6 +30,8 @@ namespace adi{
                 const double computeMeanEucledianDistance();
                 const double computeMeanDescriptorDistance();
                 const Eigen::MatrixXd computeMetricDistance();
+
+                const Eigen::Matrix3d computeBasisFunctions(const std::vector<adi::Point> &point_cloud, const std::vector<std::tuple<unsigned int, unsigned int, unsigned int>> &indices_for_basis_function_computation);
         };
     }
 }
--- src/deformation_field.cpp
+++ src/deformation_field.cpp
@@ -6,8 +6,47 @@
 
 namespace adi{
     namespace deformation_field{
-        deformationField::deformationField(const std::vector<adi::Point> &source_point_cloud, const std::vector<adi::Point> &target_point_cloud, const std::vector<std::pair<adi::Point, adi::Point>> &correspondences): m_source_point_cloud(source_point_cloud), m_target_point_cloud(target_point_cloud), m_correspondences(correspondences){}
 
+        const std::vector<std::tuple<unsigned int, unsigned int, unsigned int>> getBasisFunctions(unsigned int number_of_basis_functions)
+        {
+             // Define a lambda function to calculate the sum of squares of tuple elements
+            auto sum_of_squares = [](const std::tuple<unsigned int, unsigned int, unsigned int>& t) {
+                unsigned int sum = 0;
+                sum += std::get<0>(t) * std::get<0>(t);
+                sum += std::get<1>(t) * std::get<1>(t);
+                sum += std::get<2>(t) * std::get<2>(t);
+                return sum;
+            };
+
+            std::vector<std::tuple<unsigned int, unsigned int, unsigned int>> basis_functions;
+            const unsigned int number_of_basis_functions_per_dimension = number_of_basis_functions / 3;
+            for(unsigned int i = 0;i < number_of_basis_functions_per_dimension / 3;++i)
+            {
+                for(unsigned int j = 0;j < number_of_basis_functions_per_dimension;++j)
+                {
+                    for(unsigned int k = 0;k < number_of_basis_functions_per_dimension;++k)
+                    {
+                        basis_functions.emplace_back(std::make_tuple(i,j,k));
+                    }
+                }
+            }
+
+            std::sort(basis_functions.begin(), basis_functions.end(), [&](const auto& a, const auto& b) {
+            return sum_of_squares(a) > sum_of_squares(b);});
+
+            // Take the top 1000 elements
+            std::vector<std::tuple<unsigned int, unsigned int, unsigned int>> top_1000(basis_functions.begin(), basis_functions.begin() + 1000);
+
+            return top_1000;
+        }
+
+        deformationField::deformationField(const std::vector<adi::Point> &source_point_cloud, const std::vector<adi::Point> &target_point_cloud, const std::vector<std::pair<adi::Point, adi::Point>> &correspondences): m_source_point_cloud(source_point_cloud), m_target_point_cloud(target_point_cloud), m_correspondences(correspondences){}
+        
+        void deformationField::initDeformationField(const std::vector<std::tuple<unsigned int, unsigned int, unsigned int>> &indices_for_basis_function_computation, const std::vector<adi::Point> &point_cloud)
+        {
+            Eigen::Matrix3d gradient_of_basis_functions = this->computeBasisFunctions(point_cloud, indices_for_basis_function_computation);
+        }
+        
         const double deformationField::computeMeanEucledianDistance()
         {
             double mean_eucledian_distance = 0;
@@ -46,6 +85,18 @@ namespace adi{
             }
 
             return metric_distance;
+        }
+
+        const Eigen::Matrix3d deformationField::computeBasisFunctions(const std::vector<adi::Point> &point_cloud, const std::vector<std::tuple<unsigned int, unsigned int, unsigned int>> &indices_for_basis_function_computation)
+        {
+            Eigen::Matrix3d basis_functions = Eigen::Matrix3d::Zero(point_cloud.size(), 3);
+            for(unsigned int i = 0;i < point_cloud.size();++i)
+            {
+                basis_functions(i,0) = static_cast<double>(0.5 * std::sin(point_cloud[i].s_point[0] * M_PI * std::get<0>(indices_for_basis_function_computation[i])));
+                basis_functions(i,1) = static_cast<double>(0.5 * std::sin(point_cloud[i].s_point[1] * M_PI * std::get<1>(indices_for_basis_function_computation[i])));
+                basis_functions(i,2) = static_cast<double>(0.5 * std::sin(point_cloud[i].s_point[2] * M_PI * std::get<2>(indices_for_basis_function_computation[i])));
+            }
+            return basis_functions;
         }
         
         Eigen::MatrixXd deformationField::computeSoftCorrespondences()
