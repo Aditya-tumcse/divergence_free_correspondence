23213416d7d70a2f1f2a249e8183353a03f6b5b5
Aditya-tumcse <srinivas.adi95@gmail.com>
2024-07-01 07:48:50 +0200

    feat : restructure the code base and start implementing computation ofscalart potential fuinctions

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,7 +2,8 @@
 #define CONSTANTS_HPP
 
 const double SEARCH_RADIUS = 0.003;
-const unsigned int NUMBER_OF_SAMPLE_POINTS = 3000;
+const uint32_t NUMBER_OF_SAMPLE_POINTS = 3000;
 constexpr double SIGMA = 0.01;
+const double DOMAIN_LENGTH = 1.0;
 
 #endif
--- include/deformation_field.hpp
+++ include/deformation_field.hpp
@@ -8,36 +8,17 @@
 
 namespace adi{
     namespace deformation_field{
-
-        struct Indices{
-            unsigned int s_x;
-            unsigned int s_y;
-            unsigned int s_z;
-
-            Indices() : s_x(0), s_y(0), s_z(0){}
-
-            Indices(unsigned int x, unsigned int y, unsigned int z) : s_x(x), s_y(y), s_z(z){}
-        };
-
-        std::vector<Indices> getBasisFunctions(const unsigned int number_of_basis_functions);
-        
-        std::vector<double> getDeformationFieldCoefficients(const unsigned int number_of_coefficients_for_df);
-        class deformationField{
+        class DeformationField{
             public:
-                deformationField(const std::vector<std::pair<adi::Point, adi::Point>> &correspondences);
+                DeformationField(const uint32_t &number_of_grid_points) : m_number_of_grid_points(number_of_grid_points){}
 
-                Eigen::MatrixXd computeSoftCorrespondences(const std::vector<adi::Point> &source_point_cloud, const std::vector<adi::Point> &target_point_cloud);
+                const double computeScalarPotentialFieldByIndex(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z ); 
 
+                std::vector<Eigen::Vector3d> computeScalarPotentialFieldBasis();
+                
             private:
-                const std::vector<std::pair<adi::Point, adi::Point>> &m_correspondences;
-                std::vector<double> m_deformation_field_coefficients;
-                std::vector<Indices> m_basis_functions;
-
-                const double computeMeanEucledianDistance();
-                const double computeMeanDescriptorDistance();
-                const Eigen::MatrixXd computeMetricDistance(const std::vector<adi::Point> &source_point_cloud, const std::vector<adi::Point> &target_point_cloud);
-
-                const std::vector<double> computGradientOfBasisFunctions(const std::vector<adi::Point> &point_cloud, const std::vector<Indices> &indices_for_basis_function_computation,const unsigned int &dimension);
+                const uint32_t &m_number_of_grid_points;
+        
         };
     }
 }
