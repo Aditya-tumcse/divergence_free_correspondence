28c02d5c0d105fb1d8f11498adfceb0cd3072154
Aditya-tumcse <srinivas.adi95@gmail.com>
2024-07-07 12:22:51 +0200

    feat : compute velocity basis for the pointcloud

--- include/constants.hpp
+++ include/constants.hpp
@@ -5,5 +5,6 @@ const double SEARCH_RADIUS = 0.003;
 const uint32_t NUMBER_OF_SAMPLE_POINTS = 3000;
 constexpr double SIGMA = 0.01;
 const double DOMAIN_LENGTH = 1.0;
+const uint32_t MAX_NUMBER_OF_VELOCITY_BASIS = 27;
 
 #endif
--- include/deformation_field.hpp
+++ include/deformation_field.hpp
@@ -8,17 +8,73 @@
 
 namespace adi{
     namespace deformation_field{
+
+        struct BasisIndices{
+            uint32_t index_1;
+            uint32_t index_2;
+            uint32_t index_3;
+            double eigen_value;
+        };
         class DeformationField{
             public:
-                DeformationField(const uint32_t &number_of_grid_points) : m_number_of_grid_points(number_of_grid_points){}
+                DeformationField(const uint32_t &number_of_grid_points) : m_number_of_grid_points(number_of_grid_points){} 
 
-                const double computeScalarPotentialFieldByIndex(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z ); 
-
-                std::vector<Eigen::Vector3d> computeScalarPotentialFieldBasis();
+                std::vector<Eigen::Vector3d> computeVelocityField();
                 
             private:
                 const uint32_t &m_number_of_grid_points;
-        
+
+                /**
+                 * @brief Function to compute scalar potential field at every point in the domain
+                 * 
+                 * @param index_1
+                 * @param index_2
+                 * @param index3
+                 * @param x
+                 * @param y
+                 * @param z
+                 */
+                const double phi(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z );
+                
+                /**
+                 * @brief Function to compute partial derivative of scalar potential field with respect to x at every point in the domain
+                 * 
+                 * @param index_1
+                 * @param index_2
+                 * @param index3
+                 * @param x
+                 * @param y
+                 * @param z
+                 */
+                const double dphidx(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z );
+
+                /**
+                 * @brief Function to compute partial derivative of scalar potential field with respect to y at every point in the domain
+                 * 
+                 * @param index_1
+                 * @param index_2
+                 * @param index3
+                 * @param x
+                 * @param y
+                 * @param z
+                 */
+                const double dphidy(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z );
+                
+                /**
+                 * @brief Function to compute partial derivative of scalar potential field with respect to z at every point in the domain
+                 * 
+                 * @param index_1
+                 * @param index_2
+                 * @param index3
+                 * @param x
+                 * @param y
+                 * @param z
+                 */
+                const double dphidz(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z );
+
+                const std::vector<BasisIndices> GenerateBasisIndices(const uint32_t &max_number_of_velocity_basis);
+
+                const double ObtainCoefficientOfVelocityField(const double eigen_value);
         };
     }
 }
