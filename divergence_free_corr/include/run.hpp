e2e966db6cb69c10cba56c6c36343648ade12cb8
adi <aditya.srinivas@covisionmedia.ai>
2024-10-13 10:31:55 +0000

    fix : refactor the input to the repo

---
+++ divergence_free_corr/include/run.hpp
@@ -0,0 +1,12 @@
+#ifndef RUN_HPP
+#define RUN_HPP
+
+#include "io.hpp"
+#include "constants.hpp"
+#include "deformation_field.hpp"
+#include "numerics.hpp"
+#include "matching.hpp"
+
+void run(const std::string &source_cloud_path, const std::string &target_cloud_path);
+
+#endif
