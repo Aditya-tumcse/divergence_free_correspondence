3cab52468e685c981cd3e745f8ced15b58eef465
adi <aditya.srinivas@covisionmedia.ai>
2024-10-15 08:02:41 +0000

    fix : define formulae for velocity basis functions

--- divergence_free_corr/src/deformation_field.cpp
+++ divergence_free_corr/src/deformation_field.cpp
@@ -13,34 +13,19 @@ namespace adi{
             return 0.125 * sin(M_PI * index_1 * x) * sin(M_PI * index_2 * y) * sin(M_PI * index_3 * z);
         }
 
-        const double DeformationField::d2phidzdy(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
+         const double DeformationField::dphidx(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
         {
-            return 0.125 * M_PI * M_PI * index_3 * index_2* sin(M_PI * index_1 * x) * cos(M_PI * index_2 * y) * cos(M_PI * index_3 * z);
+            return 0.5 * M_PI * index_1 * cos(M_PI * index_1 * x) * sin(M_PI * index_2 * y) * sin(M_PI * index_3 * z);
         }
 
-        const double DeformationField::d2phidydz(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
+        const double DeformationField::dphidy(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
         {
-           return 0.125 * M_PI * M_PI *index_2 * index_3* sin(M_PI * index_1 * x) * cos(M_PI * index_2 * y) * cos(M_PI * index_3 * z);
+            return 0.5 * M_PI * index_2 * cos(M_PI * index_1 * x) * sin(M_PI * index_2 * y) * sin(M_PI * index_3 * z);
         }
 
-        const double DeformationField::d2phidxdz(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
+        const double DeformationField::dphidz(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
         {
-           return 0.125 * M_PI * M_PI * index_1 * index_3 * cos(M_PI * index_1 * x) * sin(M_PI * index_2 * y) * cos(M_PI * index_3 * z);
-        }
-
-        const double DeformationField::d2phidzdx(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
-        {
-            return 0.125 * M_PI * M_PI * index_3 * index_1 * cos(M_PI * index_1 * x) * sin(M_PI * index_2 * y) * cos(M_PI * index_3 * z);
-        }        
-
-        const double DeformationField::d2phidydx(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
-        {
-            return 0.125 * M_PI * M_PI * index_2 * index_1* cos(M_PI * index_1 * x) * cos(M_PI * index_2 * y) * sin(M_PI * index_3 * z);
-        }
-
-        const double DeformationField::d2phidxdy(const uint32_t &index_1, const uint32_t &index_2,const uint32_t &index_3, const double &x, const double &y, const double &z )
-        {
-            return 0.125 * M_PI * M_PI * index_1 * index_2* cos(M_PI * index_1 * x) * cos(M_PI * index_2 * y) * sin(M_PI * index_3 * z);
+            return 0.5 * M_PI * index_3 * cos(M_PI * index_1 * x) * sin(M_PI * index_2 * y) * sin(M_PI * index_3 * z);
         }
 
         std::tuple<double, double, double> DeformationField::computeVelocityBasisFunctions(const double &coeff_ak, const BasisIndices &base_index, const Eigen::Vector3d &point)
@@ -48,21 +33,15 @@ namespace adi{
             double vx = 0.0, vy = 0.0, vz = 0.0;
 
             // Compute partial derivatives of the eigenfunction phi
-            // double dphi_dx = dphidx(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
-            // double dphi_dy = dphidy(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
-            // double dphi_dz = dphidz(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
+            double dphi_dx = dphidx(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
+            double dphi_dy = dphidy(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
+            double dphi_dz = dphidz(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
             
-            double d2phi_dx_dz = d2phidxdz(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
-            double d2phi_dx_dy = d2phidxdy(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
-            double d2phi_dy_dz = d2phidydz(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
-            double d2phi_dy_dx = d2phidydx(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
-            double d2phi_dz_dy = d2phidzdy(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
-            double d2phi_dz_dx = d2phidzdx(base_index.index_1, base_index.index_2, base_index.index_3, point.x(), point.y(), point.z());
 
             // Basis function velocities
-            double v1x = 0, v1y = d2phi_dx_dz, v1z = -d2phi_dx_dy;
-            double v2x = -d2phi_dy_dz, v2y = 0, v2z = d2phi_dy_dx;
-            double v3x = d2phi_dz_dy, v3y = -d2phi_dz_dx, v3z = 0;
+            double v1x = 0, v1y = dphi_dz, v1z = -dphi_dy;
+            double v2x = -dphi_dz, v2y = 0, v2z = dphi_dx;
+            double v3x = dphi_dy, v3y = -dphi_dx, v3z = 0;
 
             vx = coeff_ak * (v1x + v2x + v3x);
             vy = coeff_ak * (v1y + v2y + v3y);
