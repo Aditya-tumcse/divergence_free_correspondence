f5680aba7e23c4f0ca0adb2038c806f63447f386
adi <aditya.srinivas@covisionmedia.ai>
2025-02-03 19:34:12 +0000

    feat : change in constants

--- divergence_free_corr/include/constants.hpp
+++ divergence_free_corr/include/constants.hpp
@@ -3,16 +3,16 @@
 
 #include <thread>
 
-const double SEARCH_RADIUS =
+constexpr double SEARCH_RADIUS =
     0.0576; // This value is to be adjusted accoring to the sampled points size
 constexpr int NUMBER_OF_SAMPLE_POINTS = 500;
 constexpr double SIGMA = 0.1;
 constexpr int MAX_NUMBER_OF_VELOCITY_BASIS = 50;
-const uint32_t MAX_NUMBER_OF_ITERS = 1; // 100
-const uint32_t NUMBER_OF_TIME_STEPS = 1000;
-const double TOLERANCE = 1e-06;
-constexpr double r0 = 0.01;
-const uint32_t MAX_NUMBER_OF_THREADS = std::thread::hardware_concurrency();
+constexpr uint32_t MAX_NUMBER_OF_ITERS = 1; // 100
+constexpr uint32_t NUMBER_OF_TIME_STEPS = 1000;
+constexpr double TOLERANCE = 1e-06;
 constexpr int TENSOR_DEPTH = 9;
+constexpr double THRESHOLD_CORR = 0.01;
+const size_t MAX_NUMBER_OF_THREADS = std::thread::hardware_concurrency();
 
 #endif // CONSTANTS_HPP
