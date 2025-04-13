cef0df200c7c2d5f71f207558924ac006b8e9b00
adi <aditya.srinivas@covisionmedia.ai>
2025-04-13 09:09:55 +0000

    fix : pass in the data folder path from cli

--- divergence_free_corr/src/main.cpp
+++ divergence_free_corr/src/main.cpp
@@ -1,13 +1,19 @@
 #include "../external/include/clue.hpp"
 #include "run.hpp"
+#include <filesystem>
 
-int main() {
-  const std::string source_input_cloud_path =
-      "/workspaces/divergence_free_correspondence/data/source_cloud.ply";
-  const std::string target_input_cloud_path =
-      "/workspaces/divergence_free_correspondence/data/target_cloud.ply";
+int main(int argc, char **argv) {
+  std::string data_folder = " ";
+  if (argc > 1) {
+    data_folder = argv[1];
+    if (!data_folder.empty() && data_folder.back() != '/')
+      data_folder += '/';
+  }
+  const std::string source_input_cloud_path = data_folder + "source_cloud.ply";
+  const std::string target_input_cloud_path = data_folder + "target_cloud.ply";
 
-  if (!source_input_cloud_path.empty() && !target_input_cloud_path.empty()) {
+  if (std::filesystem::exists(source_input_cloud_path) &&
+      std::filesystem::exists(target_input_cloud_path)) {
     LOG_INFO("Starting deformation modelling");
     adi::pointCloud source_cloud(source_input_cloud_path);
     adi::pointCloud target_cloud(target_input_cloud_path);
