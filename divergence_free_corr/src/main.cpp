4e3c7d82f5801e48d2cdd42608b86caf3d7e108f
adi <aditya.srinivas@covisionmedia.ai>
2025-04-13 08:57:11 +0000

    feat : exit program if no input files are found

--- divergence_free_corr/src/main.cpp
+++ divergence_free_corr/src/main.cpp
@@ -7,11 +7,16 @@ int main() {
   const std::string target_input_cloud_path =
       "/workspaces/divergence_free_correspondence/data/target_cloud.ply";
 
-  LOG_INFO("Starting deformation modelling");
-  adi::pointCloud source_cloud(source_input_cloud_path);
-  adi::pointCloud target_cloud(target_input_cloud_path);
+  if (!source_input_cloud_path.empty() && !target_input_cloud_path.empty()) {
+    LOG_INFO("Starting deformation modelling");
+    adi::pointCloud source_cloud(source_input_cloud_path);
+    adi::pointCloud target_cloud(target_input_cloud_path);
 
-  run(&source_cloud, &target_cloud);
+    run(&source_cloud, &target_cloud);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
+  } else {
+    LOG_INFO("No input files found. Hence exiting!");
+    return EXIT_FAILURE;
+  }
 }
