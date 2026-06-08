98e2e8ed69ddadce2480821265f7b9ecd340ea06
adi <aditya.srinivas@covisionmedia.ai>
2026-06-08 15:46:03 +0200

    feat : conan based depenedency management and build process, clean cmake and adding githooks

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps
+
+
+class DivFreeCorrespondenceConanfile(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+
+    def requirements(self):
+        self.requires("pcl/1.13.1")
+        self.requires("ceres-solver/2.2.0")
+        self.requires("eigen/3.4.0", override=True)
+
+    def configure(self):
+        # Disable heavy optional PCL deps not needed for this project
+        self.options["pcl"].with_opengl = False
+        self.options["pcl"].with_png = False
+        self.options["pcl"].with_pcap = False
+        self.options["ceres-solver"].use_cuda = False
+
+    def generate(self):
+        CMakeDeps(self).generate()
+        CMakeToolchain(self).generate()
